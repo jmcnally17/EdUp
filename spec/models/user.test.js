83b75dd972519472b926c634b907a5275e712e5d
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-27 14:49:09 +0100

    Create User model
    Create unit test for a user class. Make the mongoose schema and model.

---
+++ spec/models/user.test.js
@@ -0,0 +1,19 @@
+const User = require("../../models/user");
+require("../mongodb_helper");
+
+describe(User, () => {
+  // beforeEach((done) => {
+  //   mongoose.connection.collections.users.drop(() => {
+  //     done();
+  //   });
+  // });
+
+  it("has email and password", () => {
+    const user = new User({
+      email: "test1@test.com",
+      password: "password",
+    });
+
+    expect(user.email).toBe("test1@test.com");
+  });
+});
