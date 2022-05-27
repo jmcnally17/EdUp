52d9279928e81d4b155dc0f935a3e0b05a2d2142
Conor-Developer <c_hope@hotmail.co.uk>
2022-05-27 13:41:40 +0100

    Deploy test again

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 
 // client Build
-app.use(express.static("./client/build"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 
 // catch 404 and forward to error handler
