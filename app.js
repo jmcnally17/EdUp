5e1c93364ba9b3fbc6b5a8cac37c3e769485ce56
Conor-Developer <c_hope@hotmail.co.uk>
2022-05-30 15:57:38 +0100

    Before new auth guide

--- app.js
+++ app.js
@@ -1,29 +1,97 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 let cors = require("cors");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 let testAPIRouter = require("./routes/testApi");
+let sessionRouter = require("./routes/session");
+
+const mongoDb = "mongodb://127.0.0.1/edup_app";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 // client Build
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.use(
+  session({
+    secret: "secretcode",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/sessions", sessionRouter);
+
+app.get("/", (req, res) => res.render("index"));
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-    this.url =
-      process.env.REACT_APP_HEROKU_TEST_URL || "http://localhost:9000/testApi";
-  }
-
-  callAPI() {
-    fetch(this.url)
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch((err) => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="/log-in" method="POST">
+        <label htmlFor="username">Username</label>
+        <input name="username" placeholder="username" type="text" />
+        <label htmlFor="password">Password</label>
+        <input name="password" type="password" />
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
