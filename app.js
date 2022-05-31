cd3cca2237e8b5a9a84113c04ed1841a4aee7334
Conor-Developer <c_hope@hotmail.co.uk>
2022-05-31 23:09:54 +0100

    Testing deploy client build issue

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use("/backend/users", usersRouter);
 app.use("/backend/calendar", calendarRouter);
 
 app.get("*", (req, res) => {
-  let url = path.join(__dirname, "../client/build", "index.html");
+  let url = path.join(__dirname, "./client/build", "index.html");
   if (!url.startsWith("/app/"))
     // since we're on local windows
     url = url.substring(1);
