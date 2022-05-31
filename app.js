b53d445a7fa7d3275886fb3918bfab1a5a949792
Conor-Developer <c_hope@hotmail.co.uk>
2022-05-31 22:53:29 +0100

    Testing deploy client build issue

--- app.js
+++ app.js
@@ -49,10 +49,18 @@ app.use("/users", usersRouter);
 app.use("/sessions", sessionsRouter);
 app.use("/notices", noticesRouter);
 
-app.get("*", (_req, res) => {
-  response.set("Access-Control-Allow-Origin", "*");
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, "../client/build", "index.html");
+  if (!url.startsWith("/app/"))
+    // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
+
+// app.get("*", (_req, res) => {
+//   response.set("Access-Control-Allow-Origin", "*");
+//   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+// });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
