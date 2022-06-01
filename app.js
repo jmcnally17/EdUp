38a8bf237d457b22aff59d1acdbc4c85eba15092
Conor-Developer <c_hope@hotmail.co.uk>
2022-06-01 15:12:45 +0100

    Testing Heroku Production Version

--- app.js
+++ app.js
@@ -52,8 +52,10 @@ app.use("/backend/calendar", calendarRouter);
 app.get("*", (req, res) => {
   console.log("Hello world");
   console.log(req);
-  response.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  console.log("MAKERS!!!");
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  console.log("LAST MESSAGE");
 });
 
 // catch 404 and forward to error handler.
