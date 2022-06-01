1ff9407a463e996a4063525601363077e2fec43b
Jordan <jordanmanu@live.com.au>
2022-06-01 10:27:44 +0100

    updated description popup

--- client/src/components/calendar/Day.js
+++ client/src/components/calendar/Day.js
@@ -3,23 +3,12 @@ import dayjs from 'dayjs'
 import CalendarGlobalContext from '../../context/CalendarGlobalContext'
 import Popup from 'reactjs-popup';
 
-
 export default function Day({ day, _key, rowIdx, data }) {
-
-  const Card = ({ title }) => (
-    <div className="font-size: 12px width: 100% border-bottom 1px-solid-gray text-center">
-      <div className="content">
-        {title}
-      </div>
-    </div>
-  );
 
   function getCurrentDayClass() {
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? 'bg-blue-600 text-white rounded-full w-7'
       : ''
   }
-  const [open, setOpen] = useState(false);
-  const closeModal = () => setOpen(false);
   const {setDaySelected, setShowEventModal } = useContext(CalendarGlobalContext)
   return (
     <div className="border border-gray-200 flex flex-col">
@@ -32,10 +21,25 @@ export default function Day({ day, _key, rowIdx, data }) {
         </p>
         {data.map((event) => {
           return (
-            <Popup trigger={<button onClick={() => setOpen(o => !o)}>{event.day === day.format("DD") && event.month === day.format("MM") ? event.title : null}</button>}
-            position="top center"
-            closeOnDocumentClick>
-            <Card title={event.description} />
+            <Popup trigger={<button>{event.day === day.format("DD") && event.month === day.format("MM") ? event.title : null}</button>}
+            position="top"
+              on="click">
+              <div>
+                <div class="col s12 m7 width-2">
+                    <div class="card horizontal">
+                      <div class="card-image">
+                      </div>
+                      <div class="card-stacked">
+                        <div class="card-content">
+                          <p>{event.description}</p>
+                        </div>
+                        <div class="card-action">
+                          <a href="#">More Info</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
           </Popup>
           )
         })}
