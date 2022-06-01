8b51b773015a7b8372f54eafdef9b81e714b994a
Conor-Developer <c_hope@hotmail.co.uk>
2022-06-01 19:41:29 +0100

    Merged main - testing production version

--- app.js
+++ app.js
@@ -20,11 +20,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "development") {
-  console.log("inside if block");
   app.use(
     cors({
       origin: url, // <-- location of the react app were connecting to
@@ -53,12 +51,7 @@ app.use("/backend/users", usersRouter);
 app.use("/backend/calendar", calendarRouter);
 
 app.get("*", (req, res) => {
-  console.log("Hello world");
-  console.log(req);
-  // res.set("Access-Control-Allow-Origin", "*");
-  console.log("MAKERS!!!");
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  console.log("LAST MESSAGE");
 });
 
 // catch 404 and forward to error handler.
@@ -76,74 +69,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-
-module.exports = app;
-
-//// Yesterdays version of code
-
-// var createError = require("http-errors");
-// var express = require("express");
-// var path = require("path");
-// var logger = require("morgan");
-// let cors = require("cors");
-// const passport = require("passport");
-// const session = require("express-session");
-
-// var usersRouter = require("./routes/users");
-// var sessionsRouter = require("./routes/sessions");
-// var noticesRouter = require("./routes/notices");
-
-// var app = express();
-
-// var url = process.env.REACT_APP_HEROKU_TEST_URL || "http://localhost:3000";
-
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // client Build
-// app.use(express.static(path.join(__dirname, "client/build")));
-
-// app.get("*", (_req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
-// });
-
-// app.use(
-//   cors({
-//     origin: url, // <-- location of the react app were connecting to
-//     credentials: true,
-//   })
-// );
-
-// app.use(
-//   session({
-//     secret: "secretcode",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use(express.urlencoded({ extended: false }));
-// require("./passportConfig")(passport);
-
-// app.use("/users", usersRouter);
-// app.use("/sessions", sessionsRouter);
-// app.use("/notices", noticesRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-// });
-
-// module.exports = app;
--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -20,18 +20,6 @@ export default function Login() {
   const [loginPassword, setLoginPassword] = useState("");
   const [data, setData] = useState(null);
 
-  // const login = () => {
-  //   fetch(urlSessions, {
-  //     method: "POST",
-  //     credentials: "include",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       username: loginUsername,
-  //       password: loginPassword,
-  //     }),
-  //   }).then((res) => console.log(res));
-  // };
-
   const login = () => {
     Axios({
       method: "POST",
@@ -55,47 +43,39 @@ export default function Login() {
     });
   };
 
-  // const getUser = () => {
-  //   fetch(urlUsers, {
-  //     method: "GET",
-  //     credentials: "include",
-  //     headers: { "Content-Type": "application/json" },
-  //   })
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => setData(data));
-  // };
-
   return (
     <div>
       <div className="container">
-        
         <h1>Login</h1>
         <div className="input-field col s12">
           <h4>Username</h4>
-            
-            <input
-              placeholder="username"
-              onChange={(e) => setLoginUsername(e.target.value)}
-            ></input>
-             <h4>Password</h4>
-            <input
-              type="password"
-              placeholder="password"
-              onChange={(e) => setLoginPassword(e.target.value)}
-            ></input>
-            <button className="btn waves-effect waves-light" type="submit" name="action" onClick={login}>Submit
-              <i className="material-icons right">send</i>  
-            </button>
-            
-          </div>
-          <div>
-            <h1>Get User</h1>
-            <button onClick={getUser}>Submit</button>
-            {data ? <h1>Welcome Back {data.username}</h1> : null}
-          </div>
-      </div>  
+
+          <input
+            placeholder="username"
+            onChange={(e) => setLoginUsername(e.target.value)}
+          ></input>
+          <h4>Password</h4>
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setLoginPassword(e.target.value)}
+          ></input>
+          <button
+            className="btn waves-effect waves-light"
+            type="submit"
+            name="action"
+            onClick={login}
+          >
+            Submit
+            <i className="material-icons right">send</i>
+          </button>
+        </div>
+        <div>
+          <h1>Get User</h1>
+          <button onClick={getUser}>Submit</button>
+          {data ? <h1>Welcome Back {data.username}</h1> : null}
+        </div>
+      </div>
     </div>
   );
 }
