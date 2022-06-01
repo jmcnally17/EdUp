61641def4d648ee3774244b693d78a172a62b267
Conor-Developer <c_hope@hotmail.co.uk>
2022-06-01 13:05:56 +0100

    Testing Heroku Production Version

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "client/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // client Build
 //app.use(express.static(path.join(__dirname, "client/build")));
