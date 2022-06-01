e00a9dab91bd1062fdd657e162359a95b3ab695d
Conor-Developer <c_hope@hotmail.co.uk>
2022-06-01 12:08:47 +0100

    Works on Heroku but cannot communicate with backend, this breaks local version cookies

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ let cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
 
-// new
-//var cookieParser = require("cookie-parser");
-////
-
 var usersRouter = require("./routes/users");
 var calendarRouter = require("./routes/calendar");
 var sessionsRouter = require("./routes/sessions");
@@ -17,22 +13,23 @@ var noticesRouter = require("./routes/notices");
 
 var app = express();
 
-let url = process.env.REACT_APP_HEROKU_TEST_URL || "http://localhost:3000";
+var url = process.env.REACT_APP_HEROKU_TEST_URL || "http://localhost:3000";
 
 app.use(logger("dev"));
 app.use(express.json());
-
-// New
-//app.use(cookieParser());
-////
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Changed to "public" from "client/build"
+// client Build
 app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.use(
   cors({
-    origin: url, // <-- location of the react app we're connecting too.
+    origin: url, // <-- location of the react app were connecting to
     credentials: true,
   })
 );
@@ -56,25 +53,6 @@ app.use("/backend/notices", noticesRouter);
 app.use("/backend/users", usersRouter);
 app.use("/backend/calendar", calendarRouter);
 
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
-
-// Route handling for React
-
-// app.get("*", (req, res) => {
-//   let urls = path.join(__dirname, "./client/build", "index.html");
-//   if (!urls.startsWith("/app/"))
-//     // since we're on local windows
-//     urls = urls.substring(1);
-//   res.sendFile(urls);
-// });
-
-// app.get("*", (_req, res) => {
-//   //response.set("Access-Control-Allow-Origin", "*");
-//   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-// });
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -89,9 +67,32 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
+module.exports = app;
+
 //// Yesterdays version of code
 
-// client Build
+// var createError = require("http-errors");
+// var express = require("express");
+// var path = require("path");
+// var logger = require("morgan");
+// let cors = require("cors");
+// const passport = require("passport");
+// const session = require("express-session");
+
+// var usersRouter = require("./routes/users");
+// var sessionsRouter = require("./routes/sessions");
+// var noticesRouter = require("./routes/notices");
+
+// var app = express();
+
+// var url = process.env.REACT_APP_HEROKU_TEST_URL || "http://localhost:3000";
+
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // client Build
 // app.use(express.static(path.join(__dirname, "client/build")));
 
 // app.get("*", (_req, res) => {
@@ -122,3 +123,17 @@ module.exports = app;
 // app.use("/users", usersRouter);
 // app.use("/sessions", sessionsRouter);
 // app.use("/notices", noticesRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// });
+
+// module.exports = app;
