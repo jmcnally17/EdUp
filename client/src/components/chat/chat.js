8dcccea90e6e277bbd27c9c5acede33c275e56d0
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-02 15:36:28 +0100

    Connect socket between front and back

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 let url;
 if (process.env.REACT_APP_HEROKU_TEST_URL) {
@@ -9,21 +10,33 @@ if (process.env.REACT_APP_HEROKU_TEST_URL) {
 
 export default function Chat() {
   const [data, setData] = useState("");
-  const helloWorld = () => {
-    fetch(url, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  };
+  const [time, setTime] = useState("fetching");
 
   useEffect(() => {
-    helloWorld();
+    async function fetchTest() {
+      let response = await fetch(url);
+      response = await response.json();
+      setData(response.test);
+    }
+    fetchTest();
+  }, []);
+
+  useEffect(() => {
+    const socket = io("http://localhost:9000");
+
+    socket.on("connect", () => console.log(socket.id));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect(), 5000)
+    });
+
+    socket.on("time", (data) => setTime(data));
+    socket.on("disconnect", () => setTime("server disconnected"));
   }, []);
 
   return (
     <div>
       <h1>{data}</h1>
+      <h2>{time}</h2>
     </div>
   );
 }
