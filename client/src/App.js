55a66ea09e42ad4fef7879382c2924932037bf60
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-06-06 15:11:58 +0100

    first iteration of code generator

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Room from "./components/chat/room";
 import Payments from "./components/payments/payments";
 import Success from "./components/payments/success";
 import Axios from "axios";
+import Qrcode from "./components/qrcode/qrcode";
 
 export default function App() {
   let urlUsers;
@@ -50,6 +51,7 @@ export default function App() {
           <Route path="/chat" element={user ? <Join user={user}/> : <Navigate to='/' />} />
           <Route path="/chat/room" element={user ? <Room /> : <Navigate to='/' />} />
           <Route path="/success" element={user ? <Success /> : <Navigate to='/' />} />
+          <Route path="/qrcode" element={<Qrcode />} />
           <Route path="*" element={<Navigate to='/' replace />} />
         </Routes>
         <SchoolFooter />
