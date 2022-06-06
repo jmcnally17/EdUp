c4473de3f72bf858f337d1d16e291dbc4fdb7d46
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-06-06 16:45:01 +0100

    download functionality working. Added icons to the button

--- client/src/components/qrcode/qrcode.js
+++ client/src/components/qrcode/qrcode.js
@@ -1,15 +1,15 @@
-import QRCode from 'qrcode'
+import QRCode from 'qrcode';
 import { useState } from 'react'
 function Qrcode() {
     const [url, setUrl] = useState('')
     const [qr, setQr] = useState('')
     const GenerateQRCode = () => {
         QRCode.toDataURL(url, {
-            width: 800,
+            width: 500,
             margin: 2,
             color: {
-                // dark: '#335383FF',
-                // light: '#EEEEEEFF'
+                dark: '#335383FF',
+                light: '#EEEEEEFF'
             }
         }, (err, url) => {
             if (err) return console.error(err)
@@ -18,18 +18,30 @@ function Qrcode() {
         })
     }
     return (
-        <div className="app">
-            <h1>QR Generator</h1>
-            <input 
-                type="text"
-                placeholder="e.g. https://google.com"
-                value={url}
-                onChange={e => setUrl(e.target.value)} />
-            <button variant="contained" onClick={GenerateQRCode}>Generate</button>
+        <div className="container">
+            <h1 className="header center orange-text">QR Generator</h1>
+              <div className="row center"></div>
+              <div className="row center"></div>
+            <div className="input-field col s6">
+              <input 
+                  type="text"
+                  placeholder="Year 5 Annual play"
+                  value={url}
+                  onChange={e => setUrl(e.target.value)} />
+            </div>
+           
+            <a class="waves-effect waves-light btn" onClick={GenerateQRCode}>Generate<i class="material-icons right">build</i></a>
+            <div className="row center"></div>
+              <div className="row center"></div>
             {qr && <>
                 <img src={qr} />
-                <button variant='contained' color='success' href={qr} download="qrcode.png">Download</button>
+                <div className="row center"></div>
+                <a class="waves-effect waves-light btn" type="submit" href={qr} download="schoolqrcode.png">Download<i class="material-icons right">file_download</i></a>
             </>}
+              <div className="row center"></div>
+              <div className="row center"></div>
+              <div className="row center"></div>
+              <div className="row center"></div>
         </div>
     )
 }
