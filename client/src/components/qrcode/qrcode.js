234691872500bbc94ac5da24ca2089861d31979c
Michael Su <mailmsuu@gmail.com>
2022-06-07 11:17:30 +0100

    if logged in non admin goes on qrcode page, redirects to noticeboard

--- client/src/components/qrcode/qrcode.js
+++ client/src/components/qrcode/qrcode.js
@@ -51,37 +51,15 @@ function Qrcode({user}) {
         )
       } else if (user.admin === false) {
         return (
-          <Navigate to='/noticeboard'/>
+          <div>
+            <Navigate to='/noticeboard'/>
+          </div>
         )
       }
     } 
     return (
       <div>
-        <div className="container">
-            <h1 className="header center orange-text">QR Generator</h1>
-            <div className="row center"></div>
-            <div className="row center"></div>
-            <div className="input-field col s6">
-              <input 
-                  type="text"
-                  placeholder="Year 5 Annual play"
-                  value={url}
-                  onChange={e => setUrl(e.target.value)} 
-              />
-            </div>
-            <a class="waves-effect waves-light btn" onClick={GenerateQRCode}>Generate<i class="material-icons right">build</i></a>
-            <div className="row center"></div>
-            <div className="row center"></div>
-            {qr && <>
-                <img src={qr} />
-                <div className="row center"></div>
-                <a class="waves-effect waves-light btn" type="submit" href={qr} download="schoolqrcode.png">Download<i class="material-icons right">file_download</i></a>
-            </>}
-            <div className="row center"></div>
-            <div className="row center"></div>
-            <div className="row center"></div>
-            <div className="row center"></div>
-          </div>
+        {ifAdmin()}
       </div>
     )
 }
