0875dc406d996b2e603831cffec312743f2bb0bf
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-07 11:19:54 +0100

    Pass sidenav test

--- client/src/components/navbar/navbar.test.js
+++ client/src/components/navbar/navbar.test.js
@@ -4,8 +4,25 @@ import { render, screen, cleanup } from "@testing-library/react";
 
 afterEach(cleanup);
 
-test("Navbar has links to each page", () => {
-  render(<Sidenav />);
-  const linkOne = screen.getByRole("link", { name: "Noticeboard" });
-  expect(linkOne.getAttribute("href")).toBe("/noticeboard");
+describe("Sidenav", () => {
+  it("has links to each page", () => {
+    render(<Sidenav />);
+    const linkOne = screen.getByRole("link", { name: "home Home" });
+    expect(linkOne.getAttribute("href")).toBe("/");
+
+    const linkTwo = screen.getByRole("link", { name: "event_note Noticeboard" });
+    expect(linkTwo.getAttribute("href")).toBe("/noticeboard");
+
+    const linkThree = screen.getByRole("link", { name: "payments Payments" });
+    expect(linkThree.getAttribute("href")).toBe("/payments");
+
+    const linkFour = screen.getByRole("link", { name: "calendar_month Calendar" });
+    expect(linkFour.getAttribute("href")).toBe("/calendar");
+
+    const linkFive = screen.getByRole("link", { name: "forum Chat Rooms" });
+    expect(linkFive.getAttribute("href")).toBe("/chat");
+
+    const linkSix = screen.getByRole("link", { name: "language School Website" });
+    expect(linkSix.getAttribute("href")).toBe("http://makers.tech");
+  });
 });
