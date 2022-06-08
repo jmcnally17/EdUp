2da78963a45dc499ea031ce06afabb884fe81951
Michael Su <mailmsuu@gmail.com>
2022-06-08 11:06:27 +0100

    sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -44,55 +44,57 @@ export default function App() {
   return (
     <React.Fragment>
       <div className="App">
-        {user ? <Sidenav user={user} /> : null}
-        <Routes>
-          <Route path="/" element={<Login user={user} />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route
-            path="/noticeboard"
-            element={user ? <Noticeboard user={user} /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/noticeboard/new"
-            element={
-              user ? <New user={user} /> : <Navigate to="/noticeboard" />
-            }
-          />
-          <Route
-            path="/calendar"
-            element={user ? <Calendar user={user} /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/payments"
-            element={user ? <Payments user={user} /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/chat"
-            element={user ? <Join user={user} /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/chat/room"
-            element={user ? <Room /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/success"
-            element={user ? <Success /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/qrcode"
-            element={user ? <Qrcode user={user} /> : <Navigate to="/" />}
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-          <Route path="/updatePassword" element={user ? <UpdatePassword user={user} /> : <Navigate to="/" />} />
-          <Route
-            path="/parent-registration"
-            element={
-              user ? <ParentRegistration user={user} /> : <Navigate to="/" />
-            }
-          />
-          ;
-        </Routes>
-        <SchoolFooter />
+        <div class="flex flex-col h-screen justify-between">
+          {user ? <Sidenav user={user} /> : null}
+          <Routes>
+            <Route path="/" element={<Login user={user} />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route
+              path="/noticeboard"
+              element={user ? <Noticeboard user={user} /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/noticeboard/new"
+              element={
+                user ? <New user={user} /> : <Navigate to="/noticeboard" />
+              }
+            />
+            <Route
+              path="/calendar"
+              element={user ? <Calendar user={user} /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/payments"
+              element={user ? <Payments user={user} /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/chat"
+              element={user ? <Join user={user} /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/chat/room"
+              element={user ? <Room /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/success"
+              element={user ? <Success /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/qrcode"
+              element={user ? <Qrcode user={user} /> : <Navigate to="/" />}
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/updatePassword" element={user ? <UpdatePassword user={user} /> : <Navigate to="/" />} />
+            <Route
+              path="/parent-registration"
+              element={
+                user ? <ParentRegistration user={user} /> : <Navigate to="/" />
+              }
+            />
+            ;
+          </Routes>
+          <SchoolFooter />
+        </div>
       </div>
     </React.Fragment>
   );
